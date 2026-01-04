63fc47b414bd9bf1122eb627e30a034974e3b655
Zayaan <kzayaan@duck.com>
2026-01-04 07:57:29 +0000

    Initial commit of my Node.js app

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js in Docker!\n');
+});
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
